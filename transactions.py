699dd42d26c6243090853b733d0363586e4ab59e
HaMalak <baraka3456789@gmail.com>
2025-02-04 11:56:37 -0500

    Update transactions.py

--- transactions.py
+++ transactions.py
@@ -1,3 +1,6 @@
+# © 2024 The Nation of Tamarikemba and Kembacoin Developers  
+# © 2024 Ha Malak BN Adam Aman RA  
+
 from ecdsa_keygen import ECDSA_keygen  # Import the ECDSA_keygen class for signing and verification
 
 
